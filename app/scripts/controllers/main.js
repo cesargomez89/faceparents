d635b916f93a74a5f1691a67c742419896418dba
Cesar Gomez <cesargomez89@gmail.com>
2014-04-05 12:50:37 -0600

    deployment changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
 angular.module('guardkidsApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $('#profile-tab').click(function (e) {
-      e.preventDefault()
-      console.log('asd');
-      $(this).tab('show')
-    });
-    $('#friends-tab').click(function (e) {
-      e.preventDefault()
-      $(this).tab('show')
-    });
-    $('#filter-tab').click(function (e) {
-      e.preventDefault()
-      $(this).tab('show')
-    });
+.controller('MainCtrl', function () {
+  $('#profile-tab').click(function (e) {
+    e.preventDefault()
+    console.log('asd');
+    $(this).tab('show');
   });
+  $('#friends-tab').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+  $('#filter-tab').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+});
